e5be16ea89c5289b1a309d27afffbeeebc1da33d
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-02-14 22:23:20 +0900

    アイコン

--- app/src/main/java/jp/kusunoki/hacku2022_android/ui/YoutubeCard.kt
+++ app/src/main/java/jp/kusunoki/hacku2022_android/ui/YoutubeCard.kt
@@ -1,41 +1,29 @@
 package jp.kusunoki.hacku2022_android
 
-import android.view.RoundedCorner
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Gray
-import androidx.compose.ui.graphics.Color.Companion.LightGray
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun YoutubeCard() {
     Card(
-        shape = MaterialTheme.shapes.medium,
-        modifier = Modifier.height(223.dp).width(215.dp).padding(16.dp).wrapContentSize(),
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+            .height(223.dp)
+            .width(215.dp)
+            .padding(16.dp)
+            .wrapContentSize(),
         backgroundColor= MaterialTheme.colors.surface,
         elevation = 8.dp
     ) {
@@ -44,36 +32,43 @@ fun YoutubeCard() {
 }
 @Composable
 fun CardContent() {
-    Column {
+    Column(modifier = Modifier
+        .fillMaxWidth()) {
         Image(
             painter = painterResource(R.drawable.image),
             contentDescription = "サムネ画像",
             modifier = Modifier
                 .height(118.dp)
-                .width(216.dp),
-//            alignment = Alignment.Center,
+                .width(215.dp),
+            alignment = Alignment.Center,
             contentScale = ContentScale.Crop
         )
-        Column() { //Modifier.padding(16.dp)
-            Row(){
-                Image(
-                    painter = painterResource(id = R.drawable.circle),
-                    contentDescription = "チャンネル画像",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(32.dp)
-                        .clip(CircleShape)
-                )
-                Text(
-                    "youtuber講座",
-                    style = MaterialTheme.typography.subtitle1
-                )
-            }
+        Row(verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.circle),
+                contentDescription = "チャンネル画像",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(2.dp)
+                    .size(32.dp)
+                    .clip(CircleShape)
+            )
             Text(
-                "超人気youtuberから学ぶyoutuberになるには！",
-                style = MaterialTheme.typography.body2,
-                modifier = Modifier.fillMaxWidth()
+                "youtuber講座",
+                modifier = Modifier
+                    .padding(2.dp)
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.subtitle1
             )
         }
+        Text(
+            "超人気youtuberから学ぶyoutuberになるには！",
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
     }
 }
