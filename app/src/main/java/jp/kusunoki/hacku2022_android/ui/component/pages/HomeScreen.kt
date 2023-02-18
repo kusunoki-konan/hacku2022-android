896dbe100311b09d1736f0838b2bc2dbbb407f41
kota-shiokara <ikanoshiokara.fun@gmail.com>
2023-02-18 15:27:29 +0900

    不要なimport削除

--- app/src/main/java/jp/kusunoki/hacku2022_android/ui/component/pages/HomeScreen.kt
+++ app/src/main/java/jp/kusunoki/hacku2022_android/ui/component/pages/HomeScreen.kt
@@ -1,15 +1,11 @@
 package jp.kusunoki.hacku2022_android.ui.component.pages
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import jp.kusunoki.hacku2022_android.Greeting
-import jp.kusunoki.hacku2022_android.LocalNavController
-import jp.kusunoki.hacku2022_android.Screen
 
 @Composable
 fun HomeScreen() {
