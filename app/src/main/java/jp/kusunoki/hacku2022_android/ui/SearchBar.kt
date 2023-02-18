273ac7e4d805c5caba830acd1146e746808e532a
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-02-18 23:01:06 +0900

    検索バーを作成

---
+++ app/src/main/java/jp/kusunoki/hacku2022_android/ui/SearchBar.kt
@@ -0,0 +1,91 @@
+package jp.kusunoki.hacku2022_android.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.runtime.*
+
+@Composable
+fun SearchBar(
+    textFieldState: MutableState<TextFieldValue>,
+    onSubmit: (text: String) -> Unit,
+) {
+    val focusManager = LocalFocusManager.current
+
+    OutlinedTextField(
+        value = textFieldState.value,
+        onValueChange = { value ->
+            textFieldState.value = value
+        },
+        placeholder = { Text("動画のURLを張り付けて検索") },
+        shape = MaterialTheme.shapes.medium.copy(CornerSize(16.dp)),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),//入力したときのカラー
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "search icon",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (textFieldState.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        textFieldState.value =
+                            TextFieldValue("")
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "delete icon",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Transparent,//フォーカスされたときの下線
+            unfocusedBorderColor = Color.Transparent,//フォーカスされていないときの下線
+            cursorColor = Color.Black,//カーソル
+            backgroundColor = Color.LightGray,//背景
+            leadingIconColor = Color.Black,//虫眼鏡アイコン
+            trailingIconColor = Color.Black,//✕アイコン)
+        ),
+        keyboardActions = KeyboardActions {
+            focusManager.clearFocus()
+            onSubmit(textFieldState.value.text)
+        }
+    )
+}
--- app/src/main/java/jp/kusunoki/hacku2022_android/ui/component/pages/HomeScreen.kt
+++ app/src/main/java/jp/kusunoki/hacku2022_android/ui/component/pages/HomeScreen.kt
@@ -1,19 +1,30 @@
 package jp.kusunoki.hacku2022_android.ui.component.pages
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import jp.kusunoki.hacku2022_android.Greeting
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import jp.kusunoki.hacku2022_android.ui.SearchBar
 
 @Composable
 fun HomeScreen() {
     // TODO: Home画面
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+        color = MaterialTheme.colors.background,
     ) {
-        Greeting(name = "Home")
+        Column {
+            val textFieldState = remember { mutableStateOf(TextFieldValue("")) }
+            SearchBar(textFieldState = textFieldState) {
+                // TODO 検索バーに入力された文字でYoutubeのAPIを叩く
+            }
+        }
     }
 }
