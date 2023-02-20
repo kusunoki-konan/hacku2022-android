53b29b84f28deec7a154dc2846765ff9664ac1a2
kota-shiokara <ikanoshiokara.fun@gmail.com>
2023-02-20 23:47:49 +0900

    add timber

--- app/src/main/java/jp/kusunoki/hacku2022_android/KusunokiApp.kt
+++ app/src/main/java/jp/kusunoki/hacku2022_android/KusunokiApp.kt
@@ -2,10 +2,15 @@ package jp.kusunoki.hacku2022_android
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class KusunokiApp: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
