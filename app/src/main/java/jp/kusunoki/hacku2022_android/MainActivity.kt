dec80371915e73f5fbb2cf581382667c8075af64
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-02-24 00:46:17 +0900

    アプリ落ちます

--- app/src/main/java/jp/kusunoki/hacku2022_android/MainActivity.kt
+++ app/src/main/java/jp/kusunoki/hacku2022_android/MainActivity.kt
@@ -4,21 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -30,6 +34,11 @@ import jp.kusunoki.hacku2022_android.ui.component.pages.HistoryScreen
 import jp.kusunoki.hacku2022_android.ui.component.pages.HomeScreen
 import jp.kusunoki.hacku2022_android.ui.component.pages.VideoScreen
 import jp.kusunoki.hacku2022_android.ui.theme.Hacku2022androidTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
 
 val LocalNavController = staticCompositionLocalOf<NavHostController> {
     error("No Current NavController")
@@ -37,12 +46,19 @@ val LocalNavController = staticCompositionLocalOf<NavHostController> {
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val dao = RoomApplication.database.todoDao()
+    private var todoList = mutableStateListOf<Todo>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Hacku2022androidTheme {
                 val navController = rememberNavController()
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainScreen(todoList)
+                }
                 CompositionLocalProvider(
                     LocalNavController provides navController
                 ) {
@@ -77,8 +93,103 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 }
-
             }
+        }
+        loadTodo()
+    }
+    private fun loadTodo() {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                dao.getAll().forEach { todo ->
+                    todoList.add(todo)
+                }
+            }
+        }
+    }
+
+    private fun postTodo(title: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                dao.post(Todo(title = title))
+
+                todoList.clear()
+                loadTodo()
+            }
+        }
+    }
+
+    private fun deleteTodo(todo: Todo) {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                dao.delete(todo)
+
+                todoList.clear()
+                loadTodo()
+            }
+        }
+    }
+    @Composable
+    fun MainScreen(todoList: SnapshotStateList<Todo>) {
+        var text: String by remember { mutableStateOf("") }
+
+        Column {
+            TopAppBar(
+                title = { Text("Todo List") }
+            )
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth().weight(1f)
+            ) {
+                items(todoList) { todo ->
+                    key(todo.id) {
+                        TodoItem(todo)
+                    }
+                }
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = { it -> text = it },
+                    label = { Text("ToDo") },
+                    modifier = Modifier.wrapContentHeight().weight(1f)
+                )
+                Spacer(Modifier.size(16.dp))
+                Button(
+                    onClick = {
+                        if (text.isEmpty()) return@Button
+                        postTodo(text)
+                        text = ""
+                    },
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Text("ADD")
+                }
+            }
+
+        }
+    }
+    @Composable
+    fun TodoItem(todo: Todo) {
+        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clickable(onClick = { deleteTodo(todo) })
+        ) {
+            Text(
+                text = todo.title,
+                modifier = Modifier.fillMaxWidth().padding(bottom = 4.dp)
+            )
+            Text(
+                text = "created at: ${sdf.format(todo.created_at)}",
+                fontSize = 12.sp,
+                color = Color.LightGray,
+                textAlign = TextAlign.Right,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
--- app/src/main/java/jp/kusunoki/hacku2022_android/Todo.kt
+++ app/src/main/java/jp/kusunoki/hacku2022_android/Todo.kt
@@ -6,8 +6,7 @@ import java.util.*
 
 @Entity(tableName = "todos")
 data class Todo(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String = "",
     val created_at: Date = Date()
 )
