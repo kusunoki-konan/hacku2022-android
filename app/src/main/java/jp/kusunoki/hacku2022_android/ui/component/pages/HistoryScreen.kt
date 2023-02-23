7340a2939f736355a9ad58ded3ac316b40fe8380
Haru-330 <haruna.330romiyaneiro@gmail.com>
2023-02-23 13:58:56 +0900

    上に再生履歴を追加,下のアイコンをHistoryに変更

--- app/src/main/java/jp/kusunoki/hacku2022_android/ui/component/pages/HistoryScreen.kt
+++ app/src/main/java/jp/kusunoki/hacku2022_android/ui/component/pages/HistoryScreen.kt
@@ -1,11 +1,18 @@
 package jp.kusunoki.hacku2022_android.ui.component.pages
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import jp.kusunoki.hacku2022_android.Greeting
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HistoryScreen() {
@@ -14,6 +21,17 @@ fun HistoryScreen() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Greeting(name = "History")
+        val state = rememberScrollState()
+        Column(
+            modifier = Modifier
+                .verticalScroll(state)
+        ) {
+            Text(
+                "再生履歴",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(all = 8.dp)
+            )
+        }
     }
 }
